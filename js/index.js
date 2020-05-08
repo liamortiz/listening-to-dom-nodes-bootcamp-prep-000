1389cbae651dce14e7b76aac9a6f766e014f12e5
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-08 05:39:18 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
-// using this file is optional
-// you can also load the code snippets in using your browser's console
+const main = document.getElementById('main')
+
+main.addEventListener('click', (event) => {
+  alert("I was clicked!")
+})
+
+const input = document.querySelector('input')
+input.addEventListener('keydown', (event) => {
+  if (event.key === "g") {
+    return event.preventDefault()
+  } else {
+    console.log(event.key)
+  }
+})
